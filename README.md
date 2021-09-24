37c6f182f9347e979a010c4935ed85561d900680
dongmin7208 <sayg3o3d@gmail.com>
2021-09-24 13:38:26 +0900

    ADD:しりとり

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 `クローム拡張プログラム => React Developer Tools`
 `1.九九ゲーム`
+`2.しりとり`
+webpack!
 `browsers:['> 5% in JP'], ` 占有率!(browserslist)
 `{ "chrome": "92","ios": "14.5"}`
 開発者は重複を取り除かなければならない。
+修正事項を直したら`ビルド`を必ずしなければならない。
--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -3,10 +3,46 @@ const { Component } = React;
 
 class WordRelay extends Component {
     state = {
-        text: 'Hello, webpack',
+        word: 'すし',
+        value: '',
+        result: '',
+    };
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+            this.setState({
+                result: '正しい!',
+                word: this.state.value,
+                value: '',
+            })
+            this.input.focus();
+        } else {
+            this.setState({
+                result: '間違い',
+                value: '',
+            });
+            this.input.focus();
+        }
+    }
+    onChangeInput = (e) => {
+        this.setState({ value: e.target.value });
+
+    };
+    input;
+    onRefInput = (c) => {
+        this.input = c;
     };
     render() {
-        return <h1>{this.state.text}</h1>
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>入力！</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        )
     }
 }
 

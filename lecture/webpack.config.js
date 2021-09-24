d9c8f7867a20bc91dc05b99cb24750790f874b34
dongmin7208 <sayg3o3d@gmail.com>
2021-09-24 11:43:07 +0900

    Setting webpack+babel

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,1 +1,29 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js','.jsx']
+    },
+    entry: {
+        // app: ['./client.jsx', WordRelay.jsx]
+        app: ['./client'],
+    },
+    //엔트리 > 모듈적용 > 아웃풋
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options:{
+                presets:['@babel/preset-env', '@babel/preset-react'],
+                plugins:['@babel/plugin-proposal-class-properties'],
+            }
+        }],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+};

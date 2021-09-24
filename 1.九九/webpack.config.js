a0bb25adae0872717bb84e68f8f92440fb4cb082
dongmin7208 <sayg3o3d@gmail.com>
2021-09-24 12:57:05 +0900

    updata:webpack presets

--- 1.九九/webpack.config.js
+++ 1.九九/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const  webpack  = require('webpack');
 
 module.exports = {
     mode: 'development', // production
@@ -16,12 +17,24 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options:{
-                presets:['@babel/preset-env', '@babel/preset-react'],
-            }
+                presets:[
+                    ['@babel/preset-env',{
+                        targets: {
+                            browsers:['> 5% in JP'], 
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+            plugins: [],
+            },
         }],
-    },
-    output: {
+        },
+        plugins:[
+            new webpack.LoaderOptionsPlugin({debug: true}),
+        ],
+     output: {
+         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
     },
 };
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 `クローム拡張プログラム => React Developer Tools`
 `1.九九ゲーム`
+`browsers:['> 5% in JP'], ` 占有率!(browserslist)
+`{ "chrome": "92","ios": "14.5"}`
 開発者は重複を取り除かなければならない。

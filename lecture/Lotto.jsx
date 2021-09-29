420bc0383479ef74acddb5261ea584dc3c4fbdb9
dongmin7208 <sayg3o3d@gmail.com>
2021-09-29 12:17:24 +0900

    updata:Lotto(Hooks)

---
+++ lecture/Lotto.jsx
@@ -0,0 +1,70 @@
+import React, { Component, useState, useRef, useEffect, useMemo, useCallback } from 'react';
+import Ball from './Ball';
+
+function getWinNumbers() {
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    return [...winNumbers, bonusNumber];
+}
+
+
+const Lotto = () => {
+    const lottoNumbers = useMemo(() => getWinNumbers(), []);
+    const [winNumbers, setWinNumbers] = useState(lottoNumbers);
+    const [winBalls, setWinBalls] = useState([]);
+    const [bonus, setBonus] = useState(null);
+    const [redo, setRedo] = useState(false);
+    const timeouts = useRef([]);
+
+    useEffect(() => {
+        console.log('useEffect');
+        for (let i = 0; i < winNumbers.length - 1; i++) {
+            timeouts.current[i] = setTimeout(() => { //timeouts.current바뀌는게아님
+                setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
+            }, (i + 1) * 1000);
+        }
+        timeouts.current[6] = setTimeout(() => {
+            setBonus(winNumbers[6]);
+            setRedo(true);
+        }, 7000);
+        return () => {
+            timeouts.current.forEach((v) => {
+                clearTimeout(v);
+            })
+        }
+    }, [timeouts.current]) //input자리가 빈배열이면 componentDidMount랑 같은역할
+    //배열에 요소가있으면 componentDidMount랑 componentDidUpdate둘다수행
+    useEffect(() => {
+        console.log('lotto 数を作る.');
+    }, [winNumbers]);
+
+    const onClickRedo = useCallback(() => {
+        console.log('onClickRedo');
+        console.log(winNumbers);
+        setWinNumbers(getWinNumbers());
+        setWinBalls([]);
+        setBonus(null);
+        setRedo(false);
+        timeouts.current = []; //timeouts.current이 바뀜
+    }, [winNumbers]);
+    return (
+        <>
+            <div>あたり数</div>
+            <div id="결과창">
+                {winBalls.map((v) => <Ball key={v} number={v} />)}
+            </div>
+            <div>ボーナス数!</div>
+            {bonus && <Ball number={bonus} onClick={onClickRedo} />}
+            {redo && <button onClick={onClickRedo}>もう一度</button>}
+        </>
+    );
+}
+
+
+export default Lotto;
--- memo/lotto.md
+++ memo/lotto.md
@@ -7,3 +7,15 @@
     }
 
     let을 써서 클로저 문제가 안생긴다.
+
+    useMemo: 복잡한 함수 결괏값을 기억
+    getNumbers 계속 호출되니까 useMemo를 써줘서 그 값을 기억해줘서 계속안불러줌
+    useRef: 일반 값을 기억
+
+    useCallback : 함수를 기억하는것
+
+    훅스는 순서가 중요.
+    조건문 안에 절대 넣으면 안 되고 함수나 반복문 안에도 웬만하면 넣으면 안됨.
+    useEffect,callback,memo()안에 useState,넣으면 안뎀,
+    for() 안에는 useState를 넣어도되긴되는데 추천안함.
+    훅스들은 최상위 바깥에다 빼서 실행순서가 같게끔 만드는게 좋음.
